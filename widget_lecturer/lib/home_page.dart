0fc2aab17b45486349d7c742fc088955604c9d58
IniBukanApip <164147136+IniBukanApip@users.noreply.github.com>
2024-03-20 16:11:22 +0800

    Update Image

--- widget_lecturer/lib/home_page.dart
+++ widget_lecturer/lib/home_page.dart
@@ -60,3 +60,14 @@ class HomePage extends StatelessWidget {
                     color: const Color.fromARGB(255, 229, 243, 33)),
               ],
             ),
+           
+            const SizedBox(height: 20.0),
+            const Text(
+              '4. Image:',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Image.network(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVcJ4ylKQEQoo9lAIj5ObSeDcDk3O4BY9CfOnJRl-LSmqMSJzc5S2PNiE&s=10',
+              width: 150.0,
+              height: 150.0,
+            ),
